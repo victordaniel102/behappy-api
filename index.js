6f4d8cd4c45da0a36ac45916c64a445091e52679
Daniel Victor <daniel.victor9@hotmail.com>
2019-07-28 12:21:52 -0300

    CRUD operations

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 
 const Root = require('./routes/Root');
-const tasksRoute = require('./routes/Tasks');
+const Tasks = require('./routes/Tasks');
 
 dotenv.config();
 const PORT = process.env.PORT || 8080;
 
+mongoose.set('useFindAndModify', false);
 mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () => 
     console.log('Connected to DB')
 );
@@ -16,6 +17,6 @@ mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () =>
 app.use(express.json());
 
 app.use('/', Root)
-app.use('/tasks', tasksRoute);
+app.use('/tasks', Tasks);
 
 app.listen(PORT, () => console.log('Server is Running...'));
--- routes/Tasks.js
+++ routes/Tasks.js
@@ -21,4 +21,30 @@ router.get('/', async (req, res) => {
     res.send(taskList);
 });
 
+router.get('/:id', async (req, res) => {
+    const taskList = await Tasks.findById(req.params.id).select('-__v  -_id');
+    if (!taskList) res.status(400).send('Nenhum registro encontrado!');
+
+    res.send(taskList);
+});
+
+router.put('/:id', async (req, res) => {
+    const taskList = await Tasks.findByIdAndUpdate(
+        {_id: req.params.id},
+        { $set: {title: req.body.title, description: req.body.description} },
+        {new: true},
+    );
+
+    if (!taskList) res.status(400).send('Nenhum registro encontrado!');
+
+    res.send(taskList);
+});
+
+router.delete('/:id', async (req, res) => {
+    const taskList = await Tasks.findByIdAndDelete(req.params.id);
+    if (!taskList) res.status(400).send('Nenhum registro encontrado!');
+
+    res.send(taskList);
+});
+
 module.exports = router;
